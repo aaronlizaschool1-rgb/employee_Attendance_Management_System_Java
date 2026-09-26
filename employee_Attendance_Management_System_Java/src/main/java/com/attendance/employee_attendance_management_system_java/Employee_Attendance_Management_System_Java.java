edaba83f0e55007b15e8fa2fbdba9e9bcf542e98
aaronlizaschool1-rgb <aaronlizaschool1@gmail.com>
2026-09-26 22:50:34 +0800

    Snapshot1

---
+++ employee_Attendance_Management_System_Java/src/main/java/com/attendance/employee_attendance_management_system_java/Employee_Attendance_Management_System_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.attendance.employee_attendance_management_system_java;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Employee_Attendance_Management_System_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
